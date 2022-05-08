cd586059ec95932258e5f8ea354e1a618ea39fe9
mmxwillow <mxwierzba@gmail.com>
2022-05-08 11:40:23 +0200

    Created function for playing a single round

--- scripts.js
+++ scripts.js
@@ -10,7 +10,22 @@ function computerPlay(){
     }
 }
 
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
+function playerChoice(){
+    let playerChoice = prompt("Rock, Paper or Scissors?");
+    return playerChoice.toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === computerSelection)
+        return `It's a draw. You both chose ${playerSelection}.`;
+    else if((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper"))
+        return `You won! ${playerSelection[0].toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}.`;
+    else
+        return `You lost! ${computerSelection[0].toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}.`
+}
+
+let playerSelection = playerChoice();
+let computerSelection = computerPlay();
+
+alert(playRound(playerSelection,computerSelection));
+console.log(computerSelection);
