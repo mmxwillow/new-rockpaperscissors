edc78c8e4ec0e5e2b9d9a2e96a74f2350180681e
mmxwillow <mxwierzba@gmail.com>
2022-08-15 13:24:52 +0200

    Barebones UI

--- scripts.js
+++ scripts.js
@@ -1,3 +1,32 @@
+const buttons = Array.from(document.querySelectorAll('.btn-group'));
+const results = document.querySelector('#results');
+const score = document.querySelector('#score');
+const playAgain = document.querySelector('#play-again');
+
+playAgain.style.visibility="hidden";
+
+playAgain.addEventListener('click', () => {
+  playerScore = 0;
+  computerScore = 0;
+  results.textContent = "Pick a shape";
+  score.textContent = "";
+  playAgain.style.visibility="hidden";
+
+  buttons.forEach(button => {
+    button.disabled = false;
+  });
+});
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    results.textContent = playRound(button.textContent, computerPlay());
+    score.textContent = `You ${playerScore}:${computerScore} Computer`;
+    game();
+  });
+});
+
+
+
 function computerPlay() {
   let compChoice = Math.floor(Math.random() * 3);
   switch (compChoice) {
@@ -24,40 +53,34 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection == "scissors" && computerSelection == "paper")
   ) {
     playerScore++;
-    return `You won! ${
-      playerSelection[0].toUpperCase() + playerSelection.slice(1)
-    } beats ${computerSelection}.`;
+    return `You won! ${playerSelection[0].toUpperCase() + playerSelection.slice(1)
+      } beats ${computerSelection}.`;
   } else {
     computerScore++;
-    return `You lost! ${
-      computerSelection[0].toUpperCase() + computerSelection.slice(1)
-    } beats ${playerSelection}.`;
+    return `You lost! ${computerSelection[0].toUpperCase() + computerSelection.slice(1)
+      } beats ${playerSelection}.`;
   }
 }
 
 function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = playerChoice();
-    let computerSelection = computerPlay();
+  if (playerScore === 5 || computerScore === 5) {
+    score.textContent = "";
+    if (playerScore > computerScore)
+      results.textContent = `You won! You scored ${playerScore}. Computer scored ${computerScore}.`;
+    else if (playerScore < computerScore)
+      results.textContent = `You lost! Computer scored ${computerScore}. You scored ${playerScore}.`;
+    else results.textContent = `It's a draw! You both scored ${playerScore}.`;
 
-    alert(playRound(playerSelection, computerSelection));
+    playAgain.style.visibility="visible";
+
+    buttons.forEach(button => {
+      button.disabled = true;
+    });
   }
-  if(playerScore > computerScore)
-    alert(`You won! You scored ${playerScore}. Computer scored ${computerScore}.`);
-else if(playerScore < computerScore)
-  alert(`You lost! Computer scored ${computerScore}. You scored ${playerScore}`);
-  else alert(`It's a draw! You both scored ${playerScore}.`);
 
-  let playAgain = confirm("Do you wanna play again?");
-
-  if(playAgain) {
-      playerScore = 0;
-      computerScore = 0;
-      game();
-  }
 }
 
 let playerScore = 0;
 let computerScore = 0;
 
-game();
+//game();
